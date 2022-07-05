61827f65fd917e89ba3b13c71365dbde54031b18
Gustavo Takachi Toyota <gustavotakachitoyota@gmail.com>
2022-07-05 09:05:08 -0400

    Fix note arrow handle pointerdown event

--- src/components/pages/DisplayPage/DisplayNotes/NoteArrowHandles/NoteArrowHandle.vue
+++ src/components/pages/DisplayPage/DisplayNotes/NoteArrowHandles/NoteArrowHandle.vue
@@ -3,7 +3,7 @@
     class="note-arrow-handle"
     draggable="false"
     src="data:image/svg+xml;base64,PCFET0NUWVBFIHN2ZyBQVUJMSUMgIi0vL1czQy8vRFREIFNWRyAxLjEvL0VOIiAiaHR0cDovL3d3dy53My5vcmcvR3JhcGhpY3MvU1ZHLzEuMS9EVEQvc3ZnMTEuZHRkIj48c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHdpZHRoPSIxOHB4IiBoZWlnaHQ9IjI4cHgiIHZlcnNpb249IjEuMSI+PHBhdGggZD0ibSA2IDI2IEwgMTIgMjYgTCAxMiAxMiBMIDE4IDEyIEwgOSAxIEwgMSAxMiBMIDYgMTIgeiIgc3Ryb2tlPSIjZmZmIiBmaWxsPSIjMjliNmYyIi8+PC9zdmc+"
-    @pointerdown.stop="page.arrowCreation.start(note, $event)"
+    @pointerdown.left.stop="page.arrowCreation.start(note, $event)"
   />
 </template>
 

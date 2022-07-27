261afae82afcb3e761314a83f5f74956c75357a8
Gustavo Takachi Toyota <gustavotakachitoyota@gmail.com>
2022-07-27 15:25:31 -0400

    Improve UI code

--- src/stores/ui.ts
+++ src/stores/ui.ts
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia';
+import { BREAKPOINT_LG_MIN } from 'src/code/lib/responsive';
 import { negateProp } from 'src/code/lib/utils';
 
 export const useUI = defineStore('ui', {
@@ -18,14 +19,14 @@ export const useUI = defineStore('ui', {
     toggleLeftSidebar() {
       negateProp(this, 'leftSidebarExpanded');
 
-      if (this.leftSidebarExpanded && window.innerWidth < 992) {
+      if (this.leftSidebarExpanded && window.innerWidth < BREAKPOINT_LG_MIN) {
         this.rightSidebarExpanded = false;
       }
     },
     toggleRightSidebar() {
       negateProp(this, 'rightSidebarExpanded');
 
-      if (this.rightSidebarExpanded && window.innerWidth < 992) {
+      if (this.rightSidebarExpanded && window.innerWidth < BREAKPOINT_LG_MIN) {
         this.leftSidebarExpanded = false;
       }
     },
